4b7b2d7de9535301599c6d880b2eb0e44ab1207d
Arvin9 <594113869@qq.com>
2017-03-09 23:20:05 +0800

    修改日志路径

--- common/logger.js
+++ common/logger.js
@@ -7,7 +7,7 @@ var log4js = require('log4js');
 log4js.configure({
   appenders: [
     { type: 'console' },
-    { type: 'file', filename: '../logs/debug.log', category: 'debug' }
+    { type: 'file', filename: './logs/debug.log', category: 'debug' }
   ]
 });
 
